a57cf4e1c3bb3954cf5a3ad1a05fdb7cd786c738
TharinduDenawaka <tharindudasun1997@gmail.com>
2024-08-02 11:00:09 +0530

    MongoDB local environment config

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const userRoutes = require('./Routes/userRoutes');
 const adminRoutes = require('./Routes/adminRoutes');
 const { createInitialAdmin } = require('./Controllers/adminController');
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,15 +17,31 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
+
+
+//MongoDB Local environment
+mongoose.connect(process.env.MONGO_URI_LOCAL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => {
-    console.log('MongoDB connected')
-    createInitialAdmin();
-  })
-  .catch((err) => console.error('Could not connect to MongoDB:', err));
+       console.log('MongoDB is Connected....')
+       createInitialAdmin();
+     })
+     .catch((err) => console.error('Could not connect to MongoDB:', err));
+
+// MongoDB Atlas - cloud database
+// mongoose.connect(process.env.MONGO_URI_ATLAS, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => {
+//     console.log('MongoDB connected')
+//     createInitialAdmin();
+//   })
+//   .catch((err) => console.error('Could not connect to MongoDB:', err));
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
