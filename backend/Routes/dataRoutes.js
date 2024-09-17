452c13a4c0b06b57be604002413897cffaa707ee
TharinduDenawaka <tharindudasun1997@gmail.com>
2024-09-17 23:56:17 +0530

    delete unwanted

--- backend/Routes/dataRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { sendData } = require('../Controllers/dataController');
-const router = express.Router();
-
-router.post('/sendData',sendData);
-
-module.exports = router;
