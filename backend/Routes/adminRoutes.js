faf1a1df2133a01a2958519a4c8e4c814ae19e0f
TharinduDenawaka <tharindudasun1997@gmail.com>
2024-09-17 16:49:24 +0530

    Delete Unwanted things

--- backend/Routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registerAdmin, authAdmin } = require('../Controllers/adminController');
-
-const router = express.Router();
-
-router.post('/signupAdmin', registerAdmin);
-router.post('/loginAdmin', authAdmin);
-
-module.exports = router;
